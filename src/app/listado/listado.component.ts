4f0456f16e51194aa3be6a7da191901c90103aaf
Miguel <guerragalvezmiguel@gmail.com>
2025-03-06 10:22:50 +0100

    Creacion de la interfaz IProducto

--- src/app/listado/listado.component.ts
+++ src/app/listado/listado.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductosService } from '../productos.service';
+import { IProducto } from '../iproducto';
 
 @Component({
   selector: 'app-listado',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './listado.component.html',
   styleUrl: './listado.component.css'
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit {
+
+  productos!:IProducto[];
+
+  constructor(private servicioproductos:ProductosService) {}
+
+
+  ngOnInit(): void {
+      this.servicioproductos.obtenerTodos().subscribe(
+        (datos)=>{
+          this.productos = datos;
+          console.log(this.productos);
+          // this.productos[0].
+        }
+      );
+  }
 
 }
---
+++ src/app/productos.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+const URL_BASE="https://ejerciciostutorialesya.com/vue/datos.php";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  // Creamos una propiedad llamada httpclient
+  constructor(private httpclient:HttpClient) { }
+
+  // Implementar los métodos que necesitamos para:
+  // rescatar todos los productos
+  // rescatar un producto con un determinado id
+  // Actualizar ...
+  // Eliminar ...
+
+  obtenerTodos() {
+    // Devolvemos un objeto de Angular llamado Observable
+    return this.httpclient.get<any>(`${URL_BASE}`);
+  }
+}
